c812f7cdc57ee8f1c41cd270d1d72f302aeefafe
Ruoning117 <ruoningliu117@gmail.com>
2020-11-04 21:22:14 -0800

    DB Setup

---
+++ src/main/java/com/laioffer/cpms/db/MySQLDBUtil.java
@@ -0,0 +1,13 @@
+package com.laioffer.cpms.db;
+
+public class MySQLDBUtil {
+    private static final String INSTANCE = "cpms-instance.cwlklefwchrb.us-west-1.rds.amazonaws.com";
+    private static final String PORT_NUM = "3306";
+    public static final String DB_NAME = "cpms";
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "TEAM1_cpms";
+    public static final String URL = "jdbc:mysql://"
+            + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
+            + "?user=" + USERNAME + "&password=" + PASSWORD
+            + "&autoReconnect=true&serverTimezone=UTC";
+}
