850b4a5c11120178dcc3fd59892cfb119db477f6
DESKTOP-MV0DS1I\Feng and Siwen <fengus97@gmail.com>
2020-11-08 00:54:04 -0800

    added calendar for resident and admin view

--- cpms/src/pages/admin/Events.js
+++ cpms/src/pages/admin/Events.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import SideBarAdmin from '../../components/SideBarAdmin';
-import { Row, Col } from 'antd';
-
+import React from "react";
+import SideBarAdmin from "../../components/SideBarAdmin";
+import { Row, Col, Calendar } from "antd";
+import moment from "moment";
 
 const EventsAdmin = () => {
-    return (
-        <Row>
-            <Col span={4.5}>
-                <SideBarAdmin />
-
-            </Col>
-            <Col span={5.5}>
-                Here is Events for Admin
-            </Col>
-        </Row>
-    )
-
-}
+  return (
+    <Row>
+      <Col span={4.5}>
+        <SideBarAdmin />
+      </Col>
+      <Col span={25}>
+        <div
+          style={{
+            width: 1000,
+            height: 800,
+            border: "1px solid #d9d9d9",
+            borderRadius: 4,
+            margin: 10,
+            padding: 5,
+          }}
+        >
+          <Calendar />
+        </div>
+      </Col>
+    </Row>
+  );
+};
 
 export default EventsAdmin;
--- cpms/src/pages/resident/Calendar.js
+++ cpms/src/pages/resident/Calendar.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import { Row, Col } from 'antd';
-import SideBarResident from '../../components/SideBarResident';
+import React from "react";
+import { Row, Col, Calendar } from "antd";
+import SideBarResident from "../../components/SideBarResident";
+import moment from "moment";
 
+// const localizer = momentLocalizer(moment);
 
 const CalendarResident = () => {
-    return (
-        <Row>
-            <Col span={4.5}>
-                <SideBarResident />
-
-            </Col>
-            <Col span={5.5}>
-                Here is Calendar for Resident
-            </Col>
-        </Row>
-    )
-
-}
+  return (
+    <Row>
+      <Col span={4.5}>
+        <SideBarResident />
+      </Col>
+      <Col span={16}>
+        <div
+          style={{
+            width: 1000,
+            height: 800,
+            border: "1px solid #d9d9d9",
+            borderRadius: 4,
+            margin: 10,
+            padding: 5,
+          }}
+        >
+          <Calendar />
+        </div>
+      </Col>
+    </Row>
+  );
+};
 
 export default CalendarResident;
