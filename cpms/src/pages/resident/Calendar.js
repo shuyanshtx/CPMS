112e9e9a2c546a7f19247c1fe2ff28fb543d38d7
Lamberor <lamberor@gmail.com>
2020-11-08 23:31:16 +0800

    Change to Notice Calendar

--- cpms/src/pages/resident/Calendar.js
+++ cpms/src/pages/resident/Calendar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Calendar } from "antd";
+import { Row, Col, Calendar, Badge } from "antd";
 import SideBarResident from "../../components/SideBarResident";
 import moment from "moment";
 
@@ -14,7 +14,7 @@ const CalendarResident = () => {
       <Col span={16}>
         <div
           style={{
-            width: 1000,
+            width: 1600,
             height: 800,
             border: "1px solid #d9d9d9",
             borderRadius: 4,
@@ -22,11 +22,68 @@ const CalendarResident = () => {
             padding: 5,
           }}
         >
-          <Calendar />
+          <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender} />
         </div>
       </Col>
     </Row>
   );
 };
+
+function getListData(value) {
+  let listData;
+  switch (value.date()) {
+    case 8:
+      listData = [
+        { type: 'warning', content: 'Deadline.' },
+        { type: 'success', content: 'Room Reserved.' },
+      ];
+      break;
+    case 10:
+      listData = [
+        { type: 'warning', content: 'Deadline.' },
+        { type: 'success', content: 'Room Reserved.' },
+        { type: 'error', content: 'Maintenance.' },
+      ];
+      break;
+    case 15:
+      listData = [
+        { type: 'warning', content: 'Deadline.' },
+        { type: 'success', content: 'Room Reserved.' },
+        { type: 'error', content: 'Maintenance.' },
+      ];
+      break;
+    default:
+  }
+  return listData || [];
+}
+
+function dateCellRender(value) {
+  const listData = getListData(value);
+  return (
+    <ul className="events">
+      {listData.map(item => (
+        <li key={item.content}>
+          <Badge status={item.type} text={item.content} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function getMonthData(value) {
+  if (value.month() === 8) {
+    return 2334;
+  }
+}
+
+function monthCellRender(value) {
+  const num = getMonthData(value);
+  return num ? (
+    <div className="notes-month">
+      <section>{num}</section>
+      <span>Anual Service Fee</span>
+    </div>
+  ) : null;
+}
 
 export default CalendarResident;
