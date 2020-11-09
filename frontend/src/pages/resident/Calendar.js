af31e13dc034e7e5c10c980bd7be79e46c10e0ac
Lamberor <lamberor@gmail.com>
2020-11-09 09:01:05 +0800

    Add Calendar Components

--- frontend/src/pages/resident/Calendar.js
+++ frontend/src/pages/resident/Calendar.js
@@ -1,21 +1,89 @@
-import React from 'react';
-import { Row, Col } from 'antd';
-import SideBarResident from '../../components/SideBarResident';
+import React from "react";
+import { Row, Col, Calendar, Badge } from "antd";
+import SideBarResident from "../../components/SideBarResident";
+import moment from "moment";
 
+// const localizer = momentLocalizer(moment);
 
 const CalendarResident = () => {
-    return (
-        <Row>
-            <Col span={4.5}>
-                <SideBarResident />
+  return (
+    <Row>
+      <Col span={4.5}>
+        <SideBarResident />
+      </Col>
+      <Col span={16}>
+        <div
+          style={{
+            width: 1600,
+            height: 800,
+            border: "1px solid #d9d9d9",
+            borderRadius: 4,
+            margin: 10,
+            padding: 5,
+          }}
+        >
+          <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender} />
+        </div>
+      </Col>
+    </Row>
+  );
+};
 
-            </Col>
-            <Col span={5.5}>
-                Here is Calendar for Resident
-            </Col>
-        </Row>
-    )
+function getListData(value) {
+  let listData;
+  switch (value.date()) {
+    case 8:
+      listData = [
+        { type: 'warning', content: 'Deadline.' },
+        { type: 'success', content: 'Room Reserved.' },
+      ];
+      break;
+    case 10:
+      listData = [
+        { type: 'warning', content: 'Deadline.' },
+        { type: 'success', content: 'Room Reserved.' },
+        { type: 'error', content: 'Maintenance.' },
+      ];
+      break;
+    case 15:
+      listData = [
+        { type: 'warning', content: 'Deadline.' },
+        { type: 'success', content: 'Room Reserved.' },
+        { type: 'error', content: 'Maintenance.' },
+      ];
+      break;
+    default:
+  }
+  return listData || [];
+}
 
+function dateCellRender(value) {
+  const listData = getListData(value);
+  return (
+    <ul className="events">
+      {listData.map(item => (
+        <li key={item.content}>
+          <Badge status={item.type} text={item.content} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function getMonthData(value) {
+  if (value.month() === 8) {
+    return 2334;
+  }
+}
+
+function monthCellRender(value) {
+  const num = getMonthData(value);
+  return num ? (
+    <div className="notes-month">
+      <section>{num}</section>
+      <span>Anual Service Fee</span>
+    </div>
+  ) : null;
 }
 
 export default CalendarResident;
