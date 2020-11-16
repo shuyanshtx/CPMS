5b5a8bfe19de3a1d20a15cc1c33fe6a0b0ae95a1
Shuyan <shuyan.li@utexas.edu>
2020-11-15 23:21:34 -0500

    Account.js, has error with GET req

--- frontend/src/pages/resident/Account.js
+++ frontend/src/pages/resident/Account.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
 import { Row, Col } from 'antd';
 import { Link } from 'react-router-dom';
 import { Menu } from 'antd';
@@ -8,6 +9,33 @@ import SideBarResident from '../../components/SideBarResident';
 
 
 const AccountResident = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [address, setAddress] = useState('');
+    const [phoneNum, setPhoneNum] = useState('');
+
+    const urlGet = '/CPMS_war_exploded/resident_account';
+
+    useEffect(() => {
+        fetch(urlGet, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setName(data.name);
+                setEmail(data.email);
+                setAddress(data.unitNum);
+                setPhoneNum(data.phoneNum);
+            })
+        // // Update the document title using the browser API
+        // document.title = `You clicked ${count} times`;
+    });
+
     return (
         <Row>
             <Col span={4.5}>
@@ -29,7 +57,31 @@ const AccountResident = () => {
 
             </Col>
             <Col span={5.5}>
-                Here is Account for Resident
+                <div>
+                    <br /><br /><br /><br />
+                    Name: { name } <br />
+                    Email: { email} <br />
+                    Address: { address } <br />
+                    Phone: { phoneNum } <br />
+                    <Form
+                        name="basic"
+                        initialValues={{
+                     }}
+                    >
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input updated password!',
+                            },
+                        ]}
+                    >
+                    <Input />
+                    </Form.Item>
+                    </Form>
+                </div>
             </Col>
         </Row>
     )
